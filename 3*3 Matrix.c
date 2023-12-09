a4f02697f3e0dfb84f0f8c77b59df897f6533b74
eshant7505 <153259930+eshant7505@users.noreply.github.com>
2023-12-09 11:49:57 +0530

    Add files via upload

---
+++ 3*3 Matrix.c
@@ -0,0 +1,23 @@
+//3*3 matrix.
+
+#include <stdio.h>
+
+int main() {
+    // Define a 3x3 matrix using a 2D array
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    // Print the matrix
+    printf("Matrix:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
